b23667b0c871113454e00e3b8230f45194e8fb76
Annareddy Jaswanth <Jaswanth.A@igglobal.com>
2023-02-19 11:05:24 +0530

    testing lifecycle function

--- 15-Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c3-virtual-network.tf
+++ 15-Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c3-virtual-network.tf
@@ -1,7 +1,7 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "myvnet" {
-  #name                = "myvnet-1"
-  name                = "myvnet-2"
+  name                = "myvnet-1"
+  #name                = "myvnet-2"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name

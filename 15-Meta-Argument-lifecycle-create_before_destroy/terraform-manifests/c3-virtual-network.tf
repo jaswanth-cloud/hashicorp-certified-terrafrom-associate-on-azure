7e9fa393cb75d232d1615c61fa45d46bba6fdb39
Annareddy Jaswanth <Jaswanth.A@igglobal.com>
2023-02-19 11:13:07 +0530

    testing lifecycle function

--- 15-Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c3-virtual-network.tf
+++ 15-Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c3-virtual-network.tf
@@ -5,11 +5,12 @@ resource "azurerm_virtual_network" "myvnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
-
+/*
 # Lifecycle Changes
   lifecycle {
     create_before_destroy = true
   }
+*/
 }
 
 

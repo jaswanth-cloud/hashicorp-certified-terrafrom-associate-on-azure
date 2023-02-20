83b3c136c795dbd608ea740d43ef5a1b70793fec
Annareddy Jaswanth <Jaswanth.A@igglobal.com>
2023-02-20 09:43:27 +0530

    created the statefile in azure storage container

--- 39-Terraform-Remote-State-Datasource/project-2-app1/c1-versions.tf
+++ 39-Terraform-Remote-State-Datasource/project-2-app1/c1-versions.tf
@@ -14,8 +14,8 @@ terraform {
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name   = "terraform-storage-rg"
-    storage_account_name  = "terraformstate201"
-    container_name        = "tfstatefiles"
+    storage_account_name  = "terraformstate1984"
+    container_name        = "tfstatefile"
     key                   = "app1-terraform.tfstate"
   } 
 }

f8886d8dc2a37c566683b59d8499803edd86b005
Annareddy Jaswanth <Jaswanth.A@igglobal.com>
2023-02-26 12:03:18 +0530

    manuarlly created resoure

--- 48-Terraform-State-Import/terraform-manifests/c2-resource-group.tf
+++ 48-Terraform-State-Import/terraform-manifests/c2-resource-group.tf
@@ -1,7 +1,9 @@
 # terraform import azurerm_resource_group.example /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/example
 # terraform import azurerm_resource_group.example /subscriptions/<SUBSCRIPTION_ID>/resourceGroups/<RESOURCE_GROUP_NAME>
-# terraform import azurerm_resource_group.myrg /subscriptions/82808767-144c-4c66-a320-b30791668b0a/resourceGroups/myrg1
-/*
+# terraform import azurerm_resource_group.myrg /subscriptions/068b334d-5f61-48ea-879b-5a0e11d97110/resourceGroups/myrg1
+
+
+
 # Resource Group
 resource "azurerm_resource_group" "myrg" {
    name = "myrg1"
@@ -10,5 +12,5 @@ resource "azurerm_resource_group" "myrg" {
      "Tag1" = "My-Tag-1"
    }
 }
-*/
+
 
